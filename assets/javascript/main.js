031bb68f8e6cff0ca205c58e81be4ef1f80c1757
David Tran <dhtran@hotmail.com>
2018-06-21 23:35:59 -0500

    Cleaned up the javascript file. Beside using the autocomplete, the user can also search the city by typing in an input and clicking the search button.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,94 +1,121 @@
-var map;
-var infowindow;
+let map;
+let infowindow;
+let toptenthings = [];
+let bounds;
+let markers = [];
 
 function initMap() {
+
     infowindow = new google.maps.InfoWindow();
-    //Setting long and lat
+    //Setting default view of the map
     let city = { lat: 29.7765065, lng: -95.4201377 };
     let mapOptions = {
         zoom: 4,
         center: city
     }
-    map = new google.maps.Map(document.getElementById('mapArea'), mapOptions);
-    var service = new google.maps.places.PlacesService(map);
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
-    });
+    
+    //Initalizing map at $mapArea with the map options settings 
+    map = new google.maps.Map($('#mapArea')[0], mapOptions);
+    
+    let $cityInput = $('#cityInput')[0];
+    let autocomplete = new google.maps.places.Autocomplete($cityInput, {types: ['(cities)']});
 
-    var input = document.getElementById('cityInput');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
-
-    //Event listening for place changed
+    //Event listening for place changed which will change view of map to city selected
     autocomplete.addListener('place_changed', function () {
         infowindow.close();
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
-
-        if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-        }
-
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-        }
-
-        marker.setPosition(place.geometry.location);
-        
-
-        //Finding the top 10 things to do based on autocomplete location and using the callback function
-        service.nearbySearch({
-            location: place.geometry.location,
-            radius: 20000,
-            keyword: "Things to do in " + place.address_components[0].short_name,
-            rankby: "prominence"
-        }, callback);
+        let place = autocomplete.getPlace(); 
+        searchNearbyServices(place);
     })
+
 }
-let toptenthings = [];
-//Callback that will return top 10 results based on nearbysearch parameters
+
+function searchNearbyServices(place){
+    if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+    }
+    // If the place has a geometry but no viewport, then present it on a map based on this setting.
+    if (!place.geometry.viewport) {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+    } 
+    
+    let service = new google.maps.places.PlacesService(map);
+    //Finding services based on the set parameters of the requested city and then using a callback function
+    let cityName = place.address_components[0].short_name
+    if (cityName === "New York") {
+        cityName += " City"
+    }
+    service.nearbySearch({
+        location: place.geometry.location,
+        radius: 20000,
+        keyword: "Things to do in " + cityName,
+        rankby: "prominence",
+        zoom: 5
+    }, callback);
+}
+
+//Callback that will return top 10 results
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-        if (toptenthings.length !== 0) {
-            toptenthings = [];
-        }
-        for (var i = 0; i < 10; i++) {  
-            //Will create a marker for each JSON
-            createMarker(results[i]);
+        //Resetting the toptenthings variable and deleting any markers on the map if exist
+        clearOverlays();
+        toptenthings = [];
+        
+        bounds = new google.maps.LatLngBounds();
+        for (let i = 0; i < 10; i++) {
+            //Pushing each JSON object to the toptenthings array variable
             toptenthings.push(results[i]);
-            //Console logging to see the object
             console.log(toptenthings[i]);
         }
+        //Creating markers based on the toptenthings array variable
+        for (let i = 0; i < toptenthings.length; i++) {
+            //Create a marker each item
+            createMarker(toptenthings[i]);
+        }
+        //Fitting the map boundary to show all markers of the search 
+        map.fitBounds(bounds);
     }
 }
 
-
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
     });
-
-    //Adding a click event listener to the marker that will generate the popup infowindow 
+    markers.push(marker);
+    //Extending the boundary of the map based on marker's location
+    bounds.extend(place.geometry.location);
+    //Marker's click event listener that will generate a popup infowindow 
     google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent('<img src="' + place.icon + '" width="16" height="16"><div><strong>' + place.name + '</strong><br>' + 'Address: ' + place.vicinity + '</div><a href="#">Add to my Itinerary</a>');
+        infowindow.setContent('<img src="' + place.icon + '" width="16" height="16"><div><strong>' + place.name + '</strong><br>' + 'Address: ' + place.vicinity);
         infowindow.open(map, this);
     });
 }
 
-$(document).on('click', '#btnSearch', searchCity)
-function searchCity() {
-    let citySearch = $('#cityInput').val().trim();
-    //need to find long and lat of input
-    console.log(citySearch);
+function clearOverlays() {
+    for (var i = 0; i < markers.length; i++ ) {
+      markers[i].setMap(null);
+    }
+    markers = [];
+  }
 
-}
+$(document).on('click', '#btnSearch', function () {
+    
+    let citySearch = $('#cityInput').val().trim();
+    console.log(citySearch)
+    
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + citySearch + "&key=AIzaSyDg5NeULyIuOpXrGgUWNTAmc4Ect-SsFDU",
+        method: "GET"
+    })
+    .then(function (result) {
+        if(result.length !== 0) {
+            searchNearbyServices(result.results[0]);
+        }
+    })
+})
+
+
